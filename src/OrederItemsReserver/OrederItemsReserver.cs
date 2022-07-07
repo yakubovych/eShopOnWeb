e169139e83c574cfc2961a5cbf0e8497f4093731
Roman Yakubovych <Roman_Yakubovych@epam.com>
2022-07-07 20:16:57 +0300

    add azure function project and deploy it

---
+++ src/OrederItemsReserver/OrederItemsReserver.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+namespace OrederItemsReserver
+{
+    public static class OrederItemsReserver
+    {
+        [FunctionName("OrederItemsReserver")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            string Connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string containerName = Environment.GetEnvironmentVariable("ContainerName");
+            Stream myBlob = new MemoryStream();
+            var file = req.Form.Files["File"];
+            myBlob = file.OpenReadStream();
+            var blobClient = new BlobContainerClient(Connection, containerName);
+            var blob = blobClient.GetBlobClient(Guid.NewGuid() + file.FileName);
+            await blob.UploadAsync(myBlob);
+            return new OkObjectResult("file uploaded successfylly");
+        }
+    }
+}
